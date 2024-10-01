76fe49435d003a82b4567db48c8951dad568a7ef
Hammer <oio_qwq@proton.me>
2024-10-01 22:35:43 +0800

    Update Server.vue

--- components/home/Server.vue
+++ components/home/Server.vue
@@ -40,8 +40,28 @@ const features = generateData(startDate, 30);
 //   },
 // ])
 </script>
+<style>
+  .grid-container {
+    display: grid;
+    gap: 8px; /* 定义网格项的间距 */
+  }
+
+  /* 手机上显示8列 */
+  @media (max-width: 767px) {
+    .grid-container {
+      grid-template-columns: repeat(8, 1fr); /* 8列 */
+    }
+  }
+
+  /* 电脑上显示15列 */
+  @media (min-width: 768px) {
+    .grid-container {
+      grid-template-columns: repeat(15, 1fr); /* 15列 */
+    }
+  }
+</style>
 <template>
-    <div class="grid gap-2 sm:grid-cols-8 lg:grid-cols-15">
+    <div class="grid-container">
       <div
         v-for="item in features"
         :key="item.time"
