4706a45d135f9806bdea409ae5b6ff39a3793480
Hammer <oio_qwq@proton.me>
2024-10-02 02:55:23 +0800

    Update Server.vue

--- components/home/Server.vue
+++ components/home/Server.vue
@@ -57,6 +57,7 @@ onMounted(() => {
   <BarChart
     :data="features"
     index="date"
+    colors="Zinc"
     :categories="['ms']"
     :y-formatter="(tick, i) => {
       return typeof tick === 'number'
