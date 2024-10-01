ecc0b1926788a9550f36973326b113965a02e2e4
Hammer <oio_qwq@proton.me>
2024-10-01 16:17:42 +0800

    Update Footer.vue

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -17,9 +17,9 @@ onMounted(() => {
       >Sink</a>
       <a
         class="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0"
-        href="https://html.zone"
+        href="https://qwq.gs"
         target="_blank"
-        title="HTML.ZONE"
+        title="QWQ.GS"
       >
         &copy; {{ new Date().getFullYear() }} Products of HTML.ZONE
       </a>
@@ -28,7 +28,7 @@ onMounted(() => {
       >
         <a
           ref="email"
-          href="mailto:sink.cool$miantiao.me"
+          href="mailto:w.$qwq.gs"
           title="Email"
           class="text-gray-400 hover:text-gray-500"
         >
@@ -38,7 +38,7 @@ onMounted(() => {
           />
         </a>
         <a
-          href="https://t.me/htmlzone"
+          href="https://qwq.gs/me"
           target="_blank"
           title="Telegram"
           class="text-gray-400 hover:text-gray-500"
@@ -48,53 +48,7 @@ onMounted(() => {
             class="w-6 h-6"
           />
         </a>
-        <a
-          href="https://mt.ci"
-          target="_blank"
-          title="Blog"
-          class="text-gray-400 hover:text-gray-500"
-        >
-          <span class="sr-only">Blog</span>
-          <BloggerIcon
-            class="w-6 h-6"
-          />
-        </a>
 
-        <a
-          href="https://x.com/0xKaiBi"
-          target="_blank"
-          title="Twitter"
-          class="text-gray-400 hover:text-gray-500"
-        >
-          <span class="sr-only">Twitter</span>
-          <XIcon
-            class="w-6 h-6"
-          />
-        </a>
-
-        <a
-          href="https://miantiao.me/@chi"
-          target="_blank"
-          title="Mastodon"
-          class="text-gray-400 hover:text-gray-500"
-        >
-          <span class="sr-only">Mastodon</span>
-          <MastodonIcon
-            class="w-6 h-6"
-          />
-        </a>
-
-        <a
-          href="https://github.com/ccbikai"
-          target="_blank"
-          title="GitHub"
-          class="text-gray-400 hover:text-gray-500"
-        >
-          <span class="sr-only">GitHub</span>
-          <GitHubIcon
-            class="w-6 h-6"
-          />
-        </a>
       </span>
     </div>
   </section>
