e6af24114e27604ffc39a4835b070a9f503c9cea
Hammer <oio_qwq@proton.me>
2024-10-02 00:07:31 +0800

    Update Server.vue

--- components/home/Server.vue
+++ components/home/Server.vue
@@ -6,7 +6,7 @@ async function fetchData() {
             method: 'GET',
             mode: 'cors',
             headers: {
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'Content-Type': 'application/json',
             }
         });
         // 检查响应状态
