bd02d7e3e74451de5cc1760cc2d0694efaacbcab
Hammer <oio_qwq@proton.me>
2024-10-01 22:07:28 +0800

    Update Server.vue

--- components/home/Server.vue
+++ components/home/Server.vue
@@ -41,7 +41,7 @@ const features = generateData(startDate, 30);
 // ])
 </script>
 <template>
-    <div class="grid gap-2 md:gap-2 sm:grid-cols-15 md:grid-cols-30">
+    <div class="grid gap-2" style="grid-template-columns: repeat(15, 1fr);">
       <div
         v-for="item in features"
         :key="item.time"
